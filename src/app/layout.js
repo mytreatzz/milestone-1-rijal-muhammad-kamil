db03396ba81ddc20e2c76b8492cb08dbf4bb8d0d
rijal-m-kamil <rijalmkamil99@gmail.com>
2026-01-01 21:32:00 +0700

    feat: setup Inter font, metadata, and base layout styling

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,26 +1,23 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["400", "600", "700"],
 });
 
 export const metadata = {
-  title: "",
-  description: "",
+  title: "My Portfolio",
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua enim",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} antialiased font-(family-name:--font-inter) text-ctp-text bg-ctp-base`}
       >
         {children}
       </body>
